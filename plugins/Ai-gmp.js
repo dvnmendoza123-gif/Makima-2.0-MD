48daa87a395208f5d24d73b8491ba42fc90fdecc
felixdev <sungasfa@gmail.com>
2025-10-22 18:17:03 -0400

    Actualizar Ai-gmp.js

--- plugins/Ai-gmp.js
+++ plugins/Ai-gmp.js
@@ -6,7 +6,7 @@ let handler = async (m, { conn, text }) => {
   }
 
   try {
-    const response = await axios.get(`https://ruby-core.vercel.app/api/ai/geminis?text=`);
+    const response = await axios.get(`https://ruby-core.vercel.app/api/ai/geminis`);
     const respuesta = response.data.response;
     conn.sendMessage(m.chat, { text: respuesta });
   } catch (error) {
