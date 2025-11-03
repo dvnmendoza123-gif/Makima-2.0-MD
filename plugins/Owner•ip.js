c26806f3b61d25d94c6b3b0857544fbdff6bbb76
Mendoza OFC <dvnmendoza123@gmail.com>
2025-11-03 13:12:37 -0500

    Actualizar Owner•ip.js

--- plugins/Owner•ip.js
+++ plugins/Owner•ip.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, text }) => {
 //await m.reply('🧑🏻‍💻 Buscando...')
 let bot = '🧑🏻‍💻 Buscando....'
 conn.reply(m.chat, bot, m, rcanal, )
-  if (!text) return conn.reply(m.chat, '🩵 *Te Faltó La <Ip>*', m, fake, )
+  if (!text) return conn.reply(m.chat, '🌹 *Te Faltó La <Ip>*', m, fake, )
 
   axios.get(`http://ip-api.com/json/${text}?fields=status,message,country,countryCode,region,regionName,city,district,zip,lat,lon,timezone,isp,org,as,mobile,hosting,query`).then ((res) => {
     const data = res.data
